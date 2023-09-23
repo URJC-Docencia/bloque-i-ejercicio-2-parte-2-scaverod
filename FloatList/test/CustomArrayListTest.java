1366ddc18890ff9ae0335ab766d968264c5aa353
Sergio Cavero <sergio.cavero.diaz@gmail.com>
2023-09-23 12:01:15 +0200

    updated imports

--- FloatList/test/CustomArrayListTest.java
+++ FloatList/test/CustomArrayListTest.java
@@ -1,8 +1,6 @@
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.Assert.*;
 
 public class CustomArrayListTest {
 
